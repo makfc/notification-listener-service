caeae736ef4e8226560e25ba666485780948f221
makfc <makfc1234@gmail.com>
2021-02-27 20:12:28 +0800

    feat: add phone incoming call detect

--- app/src/main/java/com/makfc/notification_listener_service/NotificationListener.kt
+++ app/src/main/java/com/makfc/notification_listener_service/NotificationListener.kt
@@ -18,7 +18,6 @@ import java.net.URLEncoder
 class NotificationListener : NotificationListenerService() {
     companion object {
         const val TAG = BuildConfig.APPLICATION_ID
-        const val CHANNEL_ID = TAG
     }
 
     override fun onListenerConnected() {
@@ -28,87 +27,120 @@ class NotificationListener : NotificationListenerService() {
     override fun onNotificationPosted(sbn: StatusBarNotification) {
         if (sbn.packageName == "org.blokada.alarm") return
 //        if (sbn.packageName != "com.spotify.music") return
+
         val extras = sbn.notification.extras
         val template = extras.getCharSequence("android.template")
-        if (template != "android.app.Notification\$MediaStyle") return
-        Log.d(TAG, "extras template: ${template}")
-        Log.d(TAG, "onNotificationPosted: $sbn")
-        Log.d(TAG, "tickerText: ${sbn.notification.tickerText}")
-        if (extras != null) {
-            Log.d(TAG, "extras: ${extras}")
-            Log.d(
-                TAG,
-                "extras android.subText (Title): " +
-                        extras.getCharSequence("android.subText")
-            )
-            Log.d(
-                TAG, "extras android.text (Artist): " +
-                        extras.getCharSequence("android.text")
-            )
-        }
-/*        val token = extras.getParcelable<MediaSession.Token>("android.mediaSession")
-        Log.d(TAG, "extras token: ${token}")
-        if (token == null) return
-        val mediaController = MediaController(this, token)
-        Log.d(TAG, "playbackInfo: ${mediaController.playbackInfo}")
-        Log.d(TAG, "playbackState: ${mediaController.playbackState}")
-        Log.d(TAG, "metadata: ${mediaController.metadata}")
-        Log.d(TAG, "queueTitle: ${mediaController.queueTitle}")
-        Log.d(TAG, "mediaController.extras: ${mediaController.extras}")*/
+        val channelId = sbn.notification.channelId
+//        Log.d(TAG, "channelId: ${channelId}")
+        if (template == "android.app.Notification\$MediaStyle") {
+            Log.d(TAG, "extras template: ${template}")
+            Log.d(TAG, "onNotificationPosted: $sbn")
+            Log.d(TAG, "tickerText: ${sbn.notification.tickerText}")
+            if (extras != null) {
+                Log.d(TAG, "extras: ${extras}")
+                Log.d(
+                    TAG,
+                    "extras android.subText (Title): " +
+                            extras.getCharSequence("android.subText")
+                )
+                Log.d(
+                    TAG, "extras android.text (Artist): " +
+                            extras.getCharSequence("android.text")
+                )
+            }
+/*
+            val token = extras.getParcelable<MediaSession.Token>("android.mediaSession")
+            Log.d(TAG, "extras token: ${token}")
+            if (token == null) return
+            val mediaController = MediaController(this, token)
+            Log.d(TAG, "playbackInfo: ${mediaController.playbackInfo}")
+            Log.d(TAG, "playbackState: ${mediaController.playbackState}")
+            Log.d(TAG, "metadata: ${mediaController.metadata}")
+            Log.d(TAG, "queueTitle: ${mediaController.queueTitle}")
+            Log.d(TAG, "mediaController.extras: ${mediaController.extras}")
+*/
 
-        createNotificationChannel()
-        val query =
-            sbn.notification.tickerText?.toString() ?: extras.getCharSequence("android.title")
-                ?.toString()
-        val escapedQuery: String = URLEncoder.encode(query, "UTF-8")
-        val escapedQuery2: String = URLEncoder.encode("歌詞翻譯 $query", "UTF-8")
-        val uri: Uri = Uri.parse("http://www.google.com/search?q=$escapedQuery")
-        val uri2: Uri = Uri.parse("https://duckduckgo.com/?q=!ducky+$escapedQuery2")
-        val uri3: Uri = Uri.parse("http://www.google.com/search?q=$escapedQuery2")
-        val uri4: Uri = Uri.parse("https://genius.com/search?q=$escapedQuery")
-        val intent = Intent(Intent.ACTION_VIEW, uri)
-        val intent2 = Intent(Intent.ACTION_VIEW, uri2)
-        val intent3 = Intent(Intent.ACTION_VIEW, uri3)
-        val intent4 = Intent(Intent.ACTION_VIEW, uri4)
-//        val intent = Intent(Intent.ACTION_WEB_SEARCH)
-//        intent.putExtra(
-//            SearchManager.QUERY,
-//            sbn.notification.tickerText
-//        ) // query contains search string
-//        startActivity(intent)
-        val pendingIntent: PendingIntent = PendingIntent.getActivity(
-            this, 0,
-            intent, 0
-        )
-        val pendingIntent2: PendingIntent = PendingIntent.getActivity(
-            this, 0,
-            intent2, 0
-        )
-        val pendingIntent3: PendingIntent = PendingIntent.getActivity(
-            this, 0,
-            intent3, 0
-        )
-        val pendingIntent4: PendingIntent = PendingIntent.getActivity(
-            this, 0,
-            intent4, 0
-        )
+            createNotificationChannel()
+            val query =
+                sbn.notification.tickerText?.toString() ?: extras.getCharSequence("android.title")
+                    ?.toString()
+            val escapedQuery: String = URLEncoder.encode(query, "UTF-8")
+            val escapedQuery2: String = URLEncoder.encode("歌詞翻譯 $query", "UTF-8")
+            val uri: Uri = Uri.parse("http://www.google.com/search?q=$escapedQuery")
+            val uri2: Uri = Uri.parse("https://duckduckgo.com/?q=!ducky+$escapedQuery2")
+            val uri3: Uri = Uri.parse("http://www.google.com/search?q=$escapedQuery2")
+            val uri4: Uri = Uri.parse("https://genius.com/search?q=$escapedQuery")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            val intent2 = Intent(Intent.ACTION_VIEW, uri2)
+            val intent3 = Intent(Intent.ACTION_VIEW, uri3)
+            val intent4 = Intent(Intent.ACTION_VIEW, uri4)
+//            val intent = Intent(Intent.ACTION_WEB_SEARCH)
+//            intent.putExtra(
+//                SearchManager.QUERY,
+//                sbn.notification.tickerText
+//            ) // query contains search string
+//            startActivity(intent)
+            val pendingIntent: PendingIntent = PendingIntent.getActivity(
+                this, 0,
+                intent, 0
+            )
+            val pendingIntent2: PendingIntent = PendingIntent.getActivity(
+                this, 0,
+                intent2, 0
+            )
+            val pendingIntent3: PendingIntent = PendingIntent.getActivity(
+                this, 0,
+                intent3, 0
+            )
+            val pendingIntent4: PendingIntent = PendingIntent.getActivity(
+                this, 0,
+                intent4, 0
+            )
 
-        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setSmallIcon(R.mipmap.ic_launcher_round)
-            .setContentTitle(getString(R.string.google_search))
-            .setContentText(query)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            // Set the intent that will fire when the user taps the notification
-            .setContentIntent(pendingIntent)
-            .addAction(0, getString(R.string.lyric_translate_ducky), pendingIntent2)
-            .addAction(0, getString(R.string.genius_search), pendingIntent4)
-            .addAction(0, getString(R.string.lyric_translate_google), pendingIntent3)
-            .setSound(null)
+            val builder = NotificationCompat.Builder(this, R.string.channel_name.toString())
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle(getString(R.string.google_search))
+                .setContentText(query)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .addAction(0, getString(R.string.lyric_translate_ducky), pendingIntent2)
+                .addAction(0, getString(R.string.genius_search), pendingIntent4)
+                .addAction(0, getString(R.string.lyric_translate_google), pendingIntent3)
+                .setSound(null)
 //            .setAutoCancel(true)
-        with(NotificationManagerCompat.from(this)) {
-            // notificationId is a unique int for each notification that you must define
-            notify(0, builder.build())
-            Log.d(TAG, "notify: $builder")
+            with(NotificationManagerCompat.from(this)) {
+                // notificationId is a unique int for each notification that you must define
+                notify(0, builder.build())
+                Log.d(TAG, "notify: $builder")
+            }
+        } else if (channelId == "phone_incoming_call") {
+            createNotificationChannel2()
+            var query =
+                sbn.notification.tickerText?.toString() ?: extras.getCharSequence("android.title")
+                    ?.toString()
+            query = query?.replace(" ", "")
+            val escapedQuery: String = URLEncoder.encode(query, "UTF-8")
+            val uri: Uri = Uri.parse("http://www.google.com/search?q=$escapedQuery")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            val pendingIntent: PendingIntent = PendingIntent.getActivity(
+                this, 0,
+                intent, 0
+            )
+            val builder = NotificationCompat.Builder(this, R.string.channel_name2.toString())
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle(getString(R.string.google_search))
+                .setContentText(query)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setSound(null)
+//            .setAutoCancel(true)
+            with(NotificationManagerCompat.from(this)) {
+                // notificationId is a unique int for each notification that you must define
+                notify(1, builder.build())
+                Log.d(TAG, "notify: $builder")
+            }
         }
     }
 
@@ -130,10 +162,30 @@ class NotificationListener : NotificationListenerService() {
             val name = getString(R.string.channel_name)
             val descriptionText = getString(R.string.channel_description)
             val importance = NotificationManager.IMPORTANCE_LOW
-            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
-                description = descriptionText
-                setSound(null, null)
-            }
+            val channel =
+                NotificationChannel(R.string.channel_name.toString(), name, importance).apply {
+                    description = descriptionText
+                    setSound(null, null)
+                }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun createNotificationChannel2() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name2)
+            val descriptionText = getString(R.string.channel_description2)
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel =
+                NotificationChannel(R.string.channel_name2.toString(), name, importance).apply {
+                    description = descriptionText
+                    setSound(null, null)
+                }
             // Register the channel with the system
             val notificationManager: NotificationManager =
                 getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
